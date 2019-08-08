55c9c43272addb3c2939361b4a4081554ffa719e
Emma Laurent <emma.laurent.je@gmail.com>
2019-08-08 15:33:09 +0200

    created routes, controllers and views for Cocktail and Dose

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,7 +1,7 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
-  validates :description, :cocktail_id, presence: true, allow_blank: false
-  validates :ingredient_id, presence: true
+  validates :description, :cocktail, presence: true, allow_blank: false
+  validates :ingredient, presence: true
   validates :cocktail, uniqueness: { scope: :ingredient }
 end
