9636a43b9999f28cade581cf5e25c118cdb3ec9e
Emma Laurent <emma.laurent.je@gmail.com>
2019-08-11 12:13:46 +0200

    fixed responsive bit for cocktail show + testing some solutions to fix ingredients dropdown issues

--- app/javascript/components/select2.js
+++ app/javascript/components/select2.js
@@ -2,10 +2,6 @@ import $ from "jquery";
 import select2 from "select2";
 import 'select2/dist/css/select2.css';
 
-$('#mySelect2').select2({
-  dropdownParent: $('#parent')
-});
-
 const initSelect2 = () => {
   $(document).ready(function() {
       $('.ingredient-select').select2();
