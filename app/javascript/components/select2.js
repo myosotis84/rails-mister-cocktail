b373e29ca321452d1ce22fbcde171569e5182c2c
Emma Laurent <emma.laurent.je@gmail.com>
2019-08-11 11:30:24 +0200

    another try to fix select2

--- app/javascript/components/select2.js
+++ app/javascript/components/select2.js
@@ -2,6 +2,9 @@ import $ from "jquery";
 import select2 from "select2";
 import 'select2/dist/css/select2.css';
 
+$(() => {
+  $('.select2-enable').select2();
+});
 
 const initSelect2 = () => {
   $(document).ready(function() {
